bef3e32ffefaaf96ced8fdea7fd92037169a6259
Staid123 <pavlenkokirill120@gmail.com>
2024-08-30 22:12:03 +0300

    router connection

---
+++ service/subscription/routers/__init__.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends
+# from fastapi.security import HTTPBearer
+from subscription.routers.plan_router import router as plan_router
+# from subscription.routers.service_router import router as service_router
+# from subscription.routers.subscription_router import router as subscription_router
+
+
+# интерфейс для введения токена (который автоматически отправляеятся в заголовки) после логина
+# http_bearer = HTTPBearer(auto_error=False)
+
+# router = APIRouter(dependencies=[Depends(http_bearer)])
+router = APIRouter(prefix="/api/v1")
+
+
+router.include_router(plan_router)
+# router.include_router(service_router)
+# router.include_router(subscription_router)
