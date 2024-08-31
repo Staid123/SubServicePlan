331e18b91b4eebff28d9ee2cd070212575d7bb6a
Kirill <pavlenkokirill120@gmail.com>
2024-08-31 19:23:28 +0300

    testing plan endpoints

--- service/tests/test_plan.py
+++ service/tests/test_plan.py
@@ -2,15 +2,49 @@
 
 async def test_create_plan(ac):
     response = await ac.post(
-        url="/plan/",
+        url="/api/v1/plan/",
         json={
             "plan_type": "full",
             "discount_percent": 0
         }
     )
-    assert response.status_code == 201, "Bad status code"
+    assert response.status_code == 201
     assert response.json() == {
             "id": 1,
             "plan_type": "full",
             "discount_percent": 0
         }
+    
+async def test_get_all_plans(ac):
+    response = await ac.get(url="/api/v1/plan/all/")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 1,
+            "plan_type": "full",
+            "discount_percent": 0
+        }
+    ]
+
+async def test_get_plan_by_id(ac):
+    response = await ac.get(url="/api/v1/plan/1/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "plan_type": "full",
+        "discount_percent": 0
+    }
+
+async def test_update_plan(ac):
+    response = await ac.patch(
+        url="/api/v1/plan/1/", 
+        json={
+            "discount_percent": 1
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() ==  {
+        "id": 1,
+        "plan_type": "full",
+        "discount_percent": 1
+    }
